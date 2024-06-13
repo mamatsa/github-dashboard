d6bbb40eccf8b2847e6c83440fa1fdb6e651a7fc
mamatsa <otar.mamatsashvili@gmail.com>
2024-06-13 16:04:21 +0400

    feat: display additional data about PRs in a list

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,4 @@
 import { Octokit } from "octokit";
-import Image from "next/image";
 import Link from "next/link";
 
 export const revalidate = 0;
@@ -17,11 +16,20 @@ export default async function Home() {
 
   // Count how many tokens are in the response body
   const extraxtTokens = (body: string) => {
-    const tokensMatch = body.match(/- Number of tokens: (\d+)/);
+    const tokensMatch = body.match(/- Number of tokens\s*:?\s*(\d+)/);
     return tokensMatch ? parseInt(tokensMatch[1], 10) : 0;
   };
 
-  console.log(response.data);
+  // Count how many minutes passed since comment was created
+  const timePassed = (date: string) => {
+    const created = new Date(date);
+    const now = new Date();
+    const diff = now.getTime() - created.getTime();
+    const minutesPassed = Math.floor(diff / (1000 * 60));
+    return minutesPassed > 60
+      ? `${Math.floor(minutesPassed / 60)} hours ago`
+      : `${minutesPassed} minutes ago`;
+  };
 
   return (
     <>
@@ -32,26 +40,19 @@ export default async function Home() {
         <Link
           href={`/comments/${pr.number}`}
           key={pr.id}
-          className="border p-5 flex gap-3 items-center my-3"
+          className="border p-5 flex flex-col my-3 gap-1"
         >
-          {/* Display user name and avatar */}
-          <div>
-            <h3 className="text-sm opacity-50">{pr.user?.login}</h3>
-            {pr.user?.avatar_url && (
-              <Image
-                src={pr.user.avatar_url}
-                alt="User Avatar"
-                width={70}
-                height={70}
-              />
-            )}
+          <div className="flex items-center gap-4">
+            <h2 className="font-semibold text-lg">{pr.title}</h2>
+            <p className=" bg-green-400 px-3 py-0.5 rounded-xl text-white text-sm">
+              {pr.state}
+            </p>
           </div>
-
-          {/* Display PR title and body */}
-          <div>
-            <h2 className="font-semibold">{pr.title}</h2>
-            <p>Number of tokens: {extraxtTokens(pr.body || "")}</p>
-          </div>
+          <p>Number of tokens: {extraxtTokens(pr.body || "")}</p>
+          <p className="text-xs opacity-50 mt-1">
+            #{pr.number} opened {timePassed(pr.created_at)} ago by{" "}
+            {pr.user?.login}
+          </p>
         </Link>
       ))}
 
